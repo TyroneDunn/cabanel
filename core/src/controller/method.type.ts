90a43e9085e0cb6597503aa0ef2e67dc75209cc7
Tyrone B. Dunn <tyronebdunn@gmail.com>
2023-12-01 00:45:54 +0200

    chore: extract method type type

--- core/src/controller/method.type.ts
+++ core/src/controller/method.type.ts
@@ -2,8 +2,10 @@ import {SideEffect} from "./side-effect.type";
 import {ResponseDTO} from "../shared/response-dto.type";
 import {FeatureDTO} from "./feature-dto.type";
 
+export type MethodType = "GET" | "POST" | "PUT" | "PATCH" | "DELETE";
+
 export type Method = {
-    type: "GET" | "POST" | "PUT" | "PATCH" | "DELETE",
+    type: MethodType,
     paramKeys?: string[],
     queryParamKeys?: string[]
     sideEffects?: SideEffect[],
