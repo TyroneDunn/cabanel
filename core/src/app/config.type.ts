37981007a5ae2e177a2d32628134a7d58d12c3fc
Tyrone B. Dunn <tyronebdunn@gmail.com>
2023-11-30 17:40:10 +0200

    feat: define config type

--- core/src/app/config.type.ts
+++ core/src/app/config.type.ts
@@ -1,3 +1,26 @@
-export type Config = {}
+import {CorsOptions} from "cors";
 
-const DEFAULT_CONFIG: Config = {};
+type WebFrameworkOption = "Express" | "Nest" | "Fastify";
+type AuthStrategyOption = "Local" | "JWT";
+type DatabaseOption = "MongoDB" | "MySQL" | "GraphQL";
+type HashingAlgorithmOption = "sha512" | "lorem";
+
+export type Config = {
+    api?: WebFrameworkOption,
+    title?: string,
+    port?: number,
+    corsOptions?: CorsOptions,
+    authOptions?: {
+        strategy: AuthStrategyOption,
+        sessionSecret?: string,
+        db?: DatabaseOption
+        dbUrl?: string,
+        hashingAlgorithm?: HashingAlgorithmOption,
+        hashingIterations?: number,
+        passwordLength?: number
+        passwordSalt?: string
+    }
+}
+
+const DEFAULT_CONFIG: Config = {
+};
