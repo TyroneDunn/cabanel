d21fb0284ae18696733b5641789edf15d3b75253
Tyrone B. Dunn <tyronebdunn@gmail.com>
2023-11-30 19:47:59 +0200

    feat: define method type

--- core/src/app/method.type.ts
+++ core/src/app/method.type.ts
@@ -1,1 +1,11 @@
-export type Method = {};
+import {SideEffect} from "./side-effect.type";
+import {ParamMap} from "./param-map.type";
+import {ResponseDTO} from "./response-dto.type";
+
+export type Method<T> = {
+    type: "GET" | "POST" | "PUT" | "PATCH" | "DELETE",
+    paramsDefinition: string[],
+    parser: (paramMap: ParamMap, queryParamMap: ParamMap, payload: Object) => T,
+    sideEffects?: SideEffect<T>[],
+    callback?: (dto: T) => ResponseDTO,
+};
