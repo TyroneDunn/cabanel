59b3a68e9431d240ee764cc936c34b19d63dcc50
Tyrone B. Dunn <tyronebdunn@gmail.com>
2023-12-01 00:21:01 +0200

    refactor: expose app functions through common interface

--- core/src/app/app.ts
+++ core/src/app/app.ts
@@ -2,9 +2,16 @@ import {AppWrapper} from "./app-wrapper.interface";
 import {Config} from "./config.type";
 import {Controller} from "./controller.type";
 
-const apps: AppWrapper[] = [];
+let apps: AppWrapper[] = [];
 
-export const init = (config: Config, controllers: Controller[]): void => {};
-export const run = (): void => {
+const DEFAULT_CONFIG: Config = {};
+const init = (config: Config = DEFAULT_CONFIG, controllers: Controller[]): void => {};
+
+const run = (): void => {
     for (const app of apps) app.run();
 };
+
+export const app = {
+    init: init,
+    run: run,
+}
