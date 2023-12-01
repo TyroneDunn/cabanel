10025a0065331aadfc621a05b1606d260637fc07
Tyrone B. Dunn <tyronebdunn@gmail.com>
2023-12-01 23:09:38 +0200

    chore: clean

--- core/src/app/app-builder-facade.ts
+++ core/src/app/app-builder-facade.ts
@@ -1,8 +1,8 @@
-import {Config} from "./config.type";
-import {AppWrapper} from "./app-wrapper.interface";
 import {AppBuilder} from "./app-builder.type";
-import {expressAppBuilder} from "./express-app-builder.service";
+import {Config} from "./config.type";
 import {Controller} from "../controller/controller.type";
+import {AppWrapper} from "./app-wrapper.interface";
+import {expressAppBuilder} from "../express/express-app-builder.service";
 
 export const AppBuilderFacade: AppBuilder = {
     buildApp(config: Config, controllers: Controller[]): AppWrapper {
