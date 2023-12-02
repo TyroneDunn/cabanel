068e4ac3830195d12c39a6702d6f9dd34eb6f6de
Tyrone B. Dunn <tyronebdunn@gmail.com>
2023-12-02 12:22:41 +0200

    feat: implement password utility module

---
+++ core/src/utils/password.utility.ts
@@ -0,0 +1,33 @@
+const crypto = require('crypto');
+import {HashingAlgorithm} from "../shared/hashing-algorithm.type";
+
+export const generateHash = (
+    password: string,
+    salt: string,
+    iterations: number,
+    length: number,
+    hashingAlgorithm: HashingAlgorithm
+): string =>
+    crypto.pbkdf2Sync(
+        password,
+        salt,
+        iterations,
+        length,
+        hashingAlgorithm
+    ).toString('hex');
+
+export const validateHash = (
+    password: string,
+    hash: string,
+    salt: string,
+    iterations: number,
+    length: number,
+    hashingAlgorithm: HashingAlgorithm
+): boolean =>
+    hash === generateHash(
+        password,
+        salt,
+        iterations,
+        length,
+        hashingAlgorithm
+    );
