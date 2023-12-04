a9774116dcfcf741ea1db50255b81efaa48b080d
Tyrone B. Dunn <tyronebdunn@gmail.com>
2023-12-04 21:42:27 +0200

    feat: expose framework app functions

--- core/src/index.ts
+++ core/src/index.ts
@@ -1,6 +1,15 @@
+import {buildConfig, Config, BuildConfig as BuildConfigType} from "./app/config.type";
+import * as HTTP_STATUS_CODES from "./shared/http-status-codes.constant";
+import {Method} from "./controller/method.type";
 import {init, run} from "./app/app";
 
-export const app = {
+export const App = {
     init: init,
-    run: run,
+    run: run
 };
+
+export type AppConfig = Config;
+export type BuildConfig = BuildConfigType;
+export const buildAppConfig: BuildConfig = buildConfig;
+export type AppMethod = Method;
+export const HttpStatusCodes = HTTP_STATUS_CODES;
