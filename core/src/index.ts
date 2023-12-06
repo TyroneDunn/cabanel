18b2ca9e72df66cdfa71d066dfd781f36ecced8a
Tyrone B. Dunn <tyronebdunn@gmail.com>
2023-12-06 15:13:57 +0200

    refactor: remove http status codes export

--- core/src/index.ts
+++ core/src/index.ts
@@ -1,5 +1,4 @@
 import {buildConfig, Config, BuildConfig as BuildConfigType} from "./app/config.type";
-import * as HTTP_STATUS_CODES from "./shared/http-status-codes.constant";
 import {Method} from "./controller/method.type";
 import {init, run} from "./app/app";
 
@@ -12,4 +11,3 @@ export type AppConfig = Config;
 export type BuildConfig = BuildConfigType;
 export const buildAppConfig: BuildConfig = buildConfig;
 export type AppMethod = Method;
-export const HttpStatusCodes = HTTP_STATUS_CODES;
