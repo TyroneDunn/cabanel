c12bf195a7438fac23860f89b74c3f21508e105e
Tyrone B. Dunn <tyronebdunn@gmail.com>
2023-12-07 01:06:03 +0200

    chore: clean

--- core/src/controller/method.type.ts
+++ core/src/controller/method.type.ts
@@ -1,9 +1,6 @@
 import {SideEffect} from "./side-effect.type";
-import {ResponseDTO} from "../shared/response-dto.type";
-import {Request} from "../shared/request-dto.type";
-
-export type MethodType = "GET" | "POST" | "PUT" | "PATCH" | "DELETE";
-export type MethodCallback = (dto: Request) => Promise<ResponseDTO>;
+import {Response} from "../shared/response.type";
+import {Request} from "../shared/request.type";
 
 export type Method = {
     type: MethodType,
@@ -13,3 +10,7 @@ export type Method = {
     sideEffects: SideEffect[],
     done: MethodCallback,
 };
+
+export type MethodType = "GET" | "POST" | "PUT" | "PATCH" | "DELETE";
+export type MethodCallback = (dto: Request) => Promise<Response>;
+
