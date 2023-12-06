51fc52e616c4b5d3e5bba1907d17aa0abad556b3
Tyrone B. Dunn <tyronebdunn@gmail.com>
2023-12-06 23:29:16 +0200

    feat: extract is local strategy function

---
+++ core/src/utils/is-local-strategy.utility.ts
@@ -0,0 +1,14 @@
+import {AuthStrategy} from "../app/config.type";
+
+export const isLocalStrategy = (authStrategy: AuthStrategy) =>
+    (
+        typeof authStrategy === 'object'
+        && authStrategy !== null
+        && 'db' in authStrategy
+        && 'dbUrl' in authStrategy
+        && 'sessionSecret' in authStrategy
+        && 'hashingAlgorithm' in authStrategy
+        && 'hashingIterations' in authStrategy
+        && 'passwordLength' in authStrategy
+        && 'passwordSalt' in authStrategy
+    );
