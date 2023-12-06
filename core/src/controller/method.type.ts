014d68331f8857f7c102077bf57340168f838630
Tyrone B. Dunn <tyronebdunn@gmail.com>
2023-12-06 15:10:22 +0200

    refactor: extract method callback type

--- core/src/controller/method.type.ts
+++ core/src/controller/method.type.ts
@@ -3,6 +3,7 @@ import {ResponseDTO} from "../shared/response-dto.type";
 import {Request} from "../shared/request-dto.type";
 
 export type MethodType = "GET" | "POST" | "PUT" | "PATCH" | "DELETE";
+export type MethodCallback = (dto: Request) => Promise<ResponseDTO>;
 
 export type Method = {
     type: MethodType,
@@ -10,5 +11,5 @@ export type Method = {
     paramKeys: string[],
     queryParamKeys: string[]
     sideEffects: SideEffect[],
-    done: (dto: Request) => Promise<ResponseDTO>,
+    done: MethodCallback,
 };
