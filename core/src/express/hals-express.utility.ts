dc3c5f84818582d8538d40c207bb8ef07d2ebb5a
Tyrone B. Dunn <tyronebdunn@gmail.com>
2023-12-07 13:50:25 +0200

    refactor: implement middleware request handlers

--- core/src/express/hals-express.utility.ts
+++ core/src/express/hals-express.utility.ts
@@ -1,24 +1,40 @@
 import {
     Application as ExpressApplication,
+    NextFunction,
     Request as ExpressRequest,
     RequestHandler as ExpressRequestHandler,
     Response as ExpressResponse,
     Router as ExpressRouter
 } from "express";
 import {ParamMap as HalsParamMap} from "../app/param-map.type";
-import {Method as HalsMethod} from "../app/method.type";
+import {Method as HalsMethod, RequestHandler as HalsRequestHandler} from "../app/method.type";
 import {Request as HalsRequest} from "../app/request.type";
 import {executeSideEffects} from "../app/side-effect.utility";
 import {Response as HalsResponse} from "../app/response.type";
 import {Controller as HalsController} from "../app/controller.type";
-import {authGuard} from "./express-authentication.utility";
+import {authGuard} from "./authentication.utility";
 
-export const mapToRequestHandler = (halsMethod: HalsMethod): ExpressRequestHandler =>
+export const mapRequestHandler = (halsMethod: HalsMethod): ExpressRequestHandler =>
     async (expressRequest: ExpressRequest, expressResponse: ExpressResponse) => {
-        const halsRequest: HalsRequest = mapToHalsRequest(halsMethod, expressRequest);
-        executeSideEffects(halsRequest, halsMethod.sideEffects);
-        const halsResponse: HalsResponse = await halsMethod.done(halsRequest);
+        const halsRequest: HalsRequest = mapToHalsRequest(expressRequest, halsMethod);
+        const halsResponse: HalsResponse = await halsMethod.requestHandler(halsRequest);
         expressResponse.status(halsResponse.status).json(halsResponse);
+    };
+
+export const mapToMiddlewareRequestHandlers = (halsMethod: HalsMethod): ExpressRequestHandler[] =>
+    halsMethod.middleware.map((middleware: HalsRequestHandler) =>
+        async (expressRequest: ExpressRequest, expressResponse: ExpressResponse, next: NextFunction) => {
+            const halsRequest: HalsRequest = mapToHalsRequest(expressRequest, halsMethod);
+            const halsResponse: HalsResponse = await middleware(halsRequest);
+            if (halsResponse.error)
+                return expressResponse.status(halsResponse.status).json(halsResponse);
+            return next();
+        });
+
+const mapSideEffectsToRequestHandler = (halsMethod: HalsMethod): ExpressRequestHandler =>
+    (expressRequest: ExpressRequest, expressResponse: ExpressResponse) => {
+        const halsRequest: HalsRequest = mapToHalsRequest(expressRequest, halsMethod);
+        executeSideEffects(halsRequest, halsMethod.sideEffects);
     };
 
 export const mapToPath = (halsMethod: HalsMethod): string => {
@@ -33,7 +49,7 @@ export const mapToPath = (halsMethod: HalsMethod): string => {
     return endpoint;
 };
 
-export const mapToHalsRequest = (halsMethod: HalsMethod, expressRequest: ExpressRequest): HalsRequest => ({
+export const mapToHalsRequest = (expressRequest: ExpressRequest, halsMethod: HalsMethod): HalsRequest => ({
     paramMap: mapToHalsParamMap(halsMethod.paramKeys, expressRequest),
     queryParamMap: mapToHalsQueryParamMap(halsMethod.queryParamKeys, expressRequest),
     payload: expressRequest.body
@@ -60,21 +76,24 @@ export const mapToHalsQueryParamMap = (queryParamKeys: string[], expressRequest:
     }
     return queryParamMap;
 };
-
-export const configureExpressAppRouters =
+export const configureRouters =
     (app: ExpressApplication, halsControllers: HalsController[]): void => {
         for (const controller of halsControllers) {
             // configure router and its request handlers
             const expressRouter: ExpressRouter = ExpressRouter();
             for (const method of controller.methods) {
                 const path = mapToPath(method);
+                const sideEffects: ExpressRequestHandler = mapSideEffectsToRequestHandler(method);
+                const middlewares: ExpressRequestHandler[] = mapToMiddlewareRequestHandlers(method);
+                const requestHandler: ExpressRequestHandler = mapRequestHandler(method);
+
                 switch (method.type) {
                     case "GET": {
-                        expressRouter.get(path, mapToRequestHandler(method));
+                        expressRouter.get(path, sideEffects, ...middlewares, requestHandler);
                         break;
                     }
                     case "POST": {
-                        expressRouter.post(path, mapToRequestHandler(method));
+                        expressRouter.post(path, sideEffects, ...middlewares, requestHandler);
                         break;
                         // implement rest
                         //      ...
