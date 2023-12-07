31f4bb0e1c336a0655e0182ddcb26ddb8140226c
Tyrone B. Dunn <tyronebdunn@gmail.com>
2023-12-07 13:45:43 +0200

    refactor: simplify interface

--- core/src/express/express-app-builder.utility.ts
+++ core/src/express/express-app-builder.utility.ts
@@ -10,29 +10,27 @@ import {
 } from "express";
 const cors = require('cors');
 import {AppBuilder as HalsAppBuilder} from "../app/app-builder.utility";
-import {configureExpressAppAuthentication} from "./express-authentication.utility";
-import {configureExpressAppRouters} from "./hals-express.utility";
+import {configureAuthentication} from "./authentication.utility";
+import {configureRouters} from "./hals-express.utility";
 
 export const expressAppBuilder: HalsAppBuilder = {
     buildApp(config: HalsConfig, controllers: HalsController[]): HalsAppWrapper {
         const expressApp: ExpressApplication = express();
         expressApp.use(express.json());
         expressApp.use(cors(config.corsOptions));
-        configureExpressAppAuthentication(expressApp, config);
-        configureExpressAppRouters(expressApp, controllers);
-        expressApp.get('/', home(config.title, config.port, config.version));
-
+        configureAuthentication(expressApp, config);
+        configureRouters(expressApp, controllers);
+        expressApp.get('/', metadata(config.title, config.port, config.version));
         return {
             run: (): void => {
-                expressApp.listen(config.port,() => {
-                    console.log(`${config.title} @ port: ${config.port}`);
-                });
+                expressApp.listen(config.port,() =>
+                    console.log(`${config.title} @ port: ${config.port}`));
             }
         };
     }
 };
 
-const home =
+const metadata =
     (title: string = 'Untitled', port: number = 2400, version?: string): ExpressRequestHandler =>
         (request: ExpressRequest, response: ExpressResponse): void => {
             response.send({
