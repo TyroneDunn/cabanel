41ea5235ed595ab3c5f881b3fab0741179f53cb0
Tyrone B. Dunn <tyronebdunn@gmail.com>
2023-12-11 22:12:06 +0200

    refactor: update interface

--- core/src/index.ts
+++ core/src/index.ts
@@ -1,5 +1,9 @@
 import {init, run} from "./app/app";
-import {BuildConfig, buildConfig as buildAppImpl} from "./app/config.utility";
+import {
+    BuildConfig,
+    buildConfig as buildAppImpl,
+    buildLocalAuthStrategy as buildLocalAuthStrategyImpl
+} from "./app/config.utility";
 import {appBuilder as appBuilderImpl, AppBuilder} from "./app/app-builder.utility";
 import EventEmitter from "events";
 import {HalsEventEmitter} from "./app/event-emitter.service";
@@ -11,6 +15,19 @@ export const App = {
 
 export const buildConfig: BuildConfig = buildAppImpl;
 
+export const buildLocalAuthStrategy = buildLocalAuthStrategyImpl;
+
 export const appBuilder: AppBuilder = appBuilderImpl;
 
 export const halsEventEmitter: EventEmitter = HalsEventEmitter;
+
+export const HttpStatusCodes = {
+    OK: 200,
+    CREATED: 201,
+    BAD_REQUEST: 400,
+    UNAUTHORIZED: 401,
+    FORBIDDEN: 403,
+    NOT_FOUND: 404,
+    CONFLICT: 409,
+    INTERNAL_SERVER_ERROR: 500,
+};
