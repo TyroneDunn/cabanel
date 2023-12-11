530b39b62d8ec1f68af63b55df6fa9fcf7f9d215
Tyrone B. Dunn <tyronebdunn@gmail.com>
2023-12-11 22:37:40 +0200

    refactor: simplify validation

--- core/src/users/users-dtos-validator.utility.ts
+++ core/src/users/users-dtos-validator.utility.ts
@@ -4,19 +4,16 @@ import {
 } from "./users-dtos";
 import {ValidationOutcome} from "../shared/validation-outcome.type";
 import {Error} from "../shared/error.type";
-import {AuthRepository} from "../auth/auth-repository.type";
 
 export type UsersDtosValidator = {
     validateGetUserDTO: (dto: GetUserDTO) => Promise<ValidationOutcome>,
     validateRegisterUserDTO: (dto: RegisterUserDTO) => Promise<ValidationOutcome>,
 };
 
-export const usersDtosValidator = (usersRepository: AuthRepository): UsersDtosValidator => ({
+export const usersDtosValidator = (): UsersDtosValidator => ({
     validateGetUserDTO: async (dto: GetUserDTO): Promise<ValidationOutcome> => {
         const errors: Error[] = [];
         if (!dto.username) errors.push({type: "BadRequest", message: 'Username required.'});
-        if (!(await usersRepository.exists(dto.username))) errors.push({type: "NotFound", message: 'User not' +
-                ' found.'});
         return {errors: errors};
     },
 
@@ -24,8 +21,6 @@ export const usersDtosValidator = (usersRepository: AuthRepository): UsersDtosVa
         const errors: Error[] = [];
         if (!dto.username) errors.push({type: "BadRequest", message: 'Username required.'});
         if (!dto.password) errors.push({type: "BadRequest", message: 'Password required.'});
-        if (await usersRepository.exists(dto.username))
-            errors.push({type: "Conflict", message: 'Username already taken.'});
         return {...errors && {errors: errors}};
     },
 });
