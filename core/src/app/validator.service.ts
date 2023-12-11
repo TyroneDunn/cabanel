06d279a236d28d730f47af7d1057f0702a13e15b
Tyrone B. Dunn <tyronebdunn@gmail.com>
2023-12-11 22:29:40 +0200

    refactor: update error dependency

--- core/src/app/validator.service.ts
+++ core/src/app/validator.service.ts
@@ -1,5 +1,6 @@
 import {ValidationOutcome} from "../shared/validation-outcome.type";
 import {Config} from "./config.type";
+import {Error} from "../shared/error.type";
 
 export const validateAppConfig = (config: Config): ValidationOutcome => {
     const errors: Error[] = [];
