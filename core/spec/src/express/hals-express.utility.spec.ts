a2a41a6113928b2386fe1d173c5ebf7623295f9a
Tyrone B. Dunn <tyronebdunn@gmail.com>
2023-12-11 22:38:07 +0200

    refactor: improve test documentation

--- core/spec/src/express/hals-express.utility.spec.ts
+++ core/spec/src/express/hals-express.utility.spec.ts
@@ -6,45 +6,67 @@ import {RequestHandler} from "../../../src/app/method.type";
 const mockRequestHandler: RequestHandler = jasmine.createSpy('GetXRequestHandler');
 
 describe('Map To Path Suite', () => {
-    it('should pass', () => {
-        const mockMethod: Method = {
-            type: "GET",
-            paramKeys: ['id'],
-            queryParamKeys: [],
-            sideEffects: [],
-            middleware: [],
-            requestHandler: mockRequestHandler
-        };
-        const path: string = mapToPath(mockMethod);
-        expect(path).toBe('/:id');
+
+    describe('When given a method with no path or param key', () => {
+        it('should produce an empty path', () => {
+            const mockMethod: Method = {
+                type: "GET",
+                paramKeys: [],
+                queryParamKeys: [],
+                sideEffects: [],
+                middleware: [],
+                requestHandler: mockRequestHandler
+            };
+            const path: string = mapToPath(mockMethod);
+            expect(path).toBe('/');
+        });
     });
 
-    it('should pass', () => {
-        const mockMethod: Method = {
-            type: "GET",
-            path: 'x',
-            paramKeys: ['id'],
-            queryParamKeys: [],
-            sideEffects: [],
-            middleware: [],
-            requestHandler: mockRequestHandler
-        };
-        const path: string = mapToPath(mockMethod);
-        expect(path).toBe('/x/:id');
+    describe('When given a method with a single param key', () => {
+        it('should produce a path with a single param', () => {
+            const mockMethodWithSingleParam: Method = {
+                type: "GET",
+                paramKeys: ['id'],
+                queryParamKeys: [],
+                sideEffects: [],
+                middleware: [],
+                requestHandler: mockRequestHandler
+            };
+            const pathWithSingleParam: string = mapToPath(mockMethodWithSingleParam);
+            expect(pathWithSingleParam).toBe('/:id');
+        });
     });
 
 
-    it('should pass', () => {
-        const mockMethod: Method = {
-            type: "GET",
-            path: 'x',
-            paramKeys: ['id', 'a'],
-            queryParamKeys: [],
-            sideEffects: [],
-            middleware: [],
-            requestHandler: mockRequestHandler
-        };
-        const path: string = mapToPath(mockMethod);
-        expect(path).toBe('/x/:id/:a');
+    describe('When given a method with a path and a single param', () => {
+        it('should produce a path with path and single param', () => {
+            const mockMethodWithPathAndSingleParam: Method = {
+                type: "GET",
+                path: 'x',
+                paramKeys: ['id'],
+                queryParamKeys: [],
+                sideEffects: [],
+                middleware: [],
+                requestHandler: mockRequestHandler
+            };
+            const pathWithPathAndSingleParam: string = mapToPath(mockMethodWithPathAndSingleParam);
+            expect(pathWithPathAndSingleParam).toBe('/x/:id');
+        });
+    });
+
+    describe('When given a method with a path and two params', () => {
+        it('should produce a path with a path and two params', () => {
+            const mockMethodWithPathAndTwoParams: Method = {
+                type: "GET",
+                path: 'x',
+                paramKeys: ['id', 'a'],
+                queryParamKeys: [],
+                sideEffects: [],
+                middleware: [],
+                requestHandler: mockRequestHandler
+            };
+            const pathWithPathAndTwoParams: string = mapToPath(mockMethodWithPathAndTwoParams);
+            expect(pathWithPathAndTwoParams).toBe('/x/:id/:a');
+        });
     });
 });
