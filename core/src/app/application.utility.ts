5645b8a2ca3cc87a331503efa27759b8611b06f4
Tyrone B. Dunn <tyronebdunn@gmail.com>
2023-12-15 00:16:45 +0200

    chore: clean

--- core/src/app/application.utility.ts
+++ core/src/app/application.utility.ts
@@ -6,28 +6,6 @@ import {ValidationOutcome} from "../shared/validation-outcome.type";
 import {throwErrors} from "../shared/error-handler.service";
 import {appBuilder} from "./app-builder.utility";
 
-const apps: AppWrapper[] = [];
-
-export type Init = (
-    config: Config,
-    controllers: Controller[]
-) => void;
-
-export const init: Init = (
-    config: Config,
-    controllers: Controller[] = []
-): void => {
-    const validationOutcome: ValidationOutcome = validateAppConfig(config);
-    if (validationOutcome.errors.length)
-        throwErrors(validationOutcome.errors.map(error => new Error(error.message)));
-    apps.push(appBuilder.buildApp(config, controllers));
-};
-
-export type Run = () => void;
-export const run: Run = (): void =>
-    apps.forEach(app => app.run());
-
-
 export type InitialiseApplication = (
     config: Schema,
     controllers: Controller[]
