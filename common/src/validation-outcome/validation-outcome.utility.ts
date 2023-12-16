a5870e18480ed95d0d32fae4e71d44ae62450f67
Tyrone B. Dunn <tyronebdunn@gmail.com>
2023-12-17 00:27:12 +0200

    refactor: update error implementation

--- common/src/validation-outcome/validation-outcome.utility.ts
+++ common/src/validation-outcome/validation-outcome.utility.ts
@@ -1,43 +1,46 @@
 import {ValidationOutcome} from "./validation-outcome.type";
 import {Response} from "../app/response.type";
 import {
-    BadRequestError,
-    ConflictError,
-    ForbiddenError,
-    NotFoundError,
-    UnauthorizedError
-} from "../errors/error.type";
-import {
     BAD_REQUEST,
     CONFLICT,
     FORBIDDEN, INTERNAL_SERVER_ERROR,
-    NOT_FOUND,
+    NOT_FOUND, OK,
     UNAUTHORIZED
 } from "../http/http-status-codes.constant";
 
 export const mapValidationOutcomeToErrorResponse = (validationOutcome: ValidationOutcome): Response => {
-    if (validationOutcome.error instanceof BadRequestError) return ({
+    if (validationOutcome.error === undefined)
+        return {
+            status: OK,
+        };
+
+    if (validationOutcome.error.type ===  "BadRequest") return ({
         status: BAD_REQUEST,
         error: validationOutcome.error.message,
     });
-    if (validationOutcome.error instanceof NotFoundError) return {
+
+    if (validationOutcome.error.type === "NotFound") return {
         status: NOT_FOUND,
         error: validationOutcome.error.message,
     };
-    if (validationOutcome.error instanceof UnauthorizedError) return {
+
+    if (validationOutcome.error.type === "Unauthorized") return {
         status: UNAUTHORIZED,
         error: validationOutcome.error.message,
     };
-    if (validationOutcome.error instanceof ForbiddenError) return {
+
+    if (validationOutcome.error.type === "Forbidden") return {
         status: FORBIDDEN,
         error: validationOutcome.error.message,
     };
-    if (validationOutcome.error instanceof ConflictError) return {
+
+    if (validationOutcome.error.type === "Conflict") return {
         status: CONFLICT,
         error: validationOutcome.error.message,
     };
+
     return {
         status: INTERNAL_SERVER_ERROR,
         error: validationOutcome.error?.message
-    }
+    };
 };
