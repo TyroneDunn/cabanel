bc89cae2b244659469872f7dca99391c840b7fa0
Tyrone B. Dunn <tyronebdunn@gmail.com>
2023-12-17 01:19:59 +0200

    feat: define password type

---
+++ common/src/password/password.type.ts
@@ -0,0 +1,6 @@
+export type HashUtility = {
+    generateHash: (password: string) => string,
+    validateHash: (password: string, hash: string) => boolean,
+};
+
+export type GenerateSalt = () => string;
