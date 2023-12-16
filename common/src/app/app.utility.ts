4653d1f10b38d0e1fda42ae57aabb88383b2aa3d
Tyrone B. Dunn <tyronebdunn@gmail.com>
2023-12-16 23:57:19 +0200

    feat: define app utility

---
+++ common/src/app/app.utility.ts
@@ -0,0 +1,8 @@
+import {Request} from "./request.type";
+import {Response} from "./response.type";
+export const addRequestPageDataToResponse = (request: Request, response: Response): Response =>
+    ({
+        ...response,
+        index: parseInt(request.queryParamMap['index']),
+        limit: parseInt(request.queryParamMap['limit']),
+    });
