d098454a2443c8ffb5444350cb5b4fa7f966ea1a
Tyrone B. Dunn <tyronebdunn@gmail.com>
2023-12-17 00:35:42 +0200

    refactor: make function total

--- common/src/page/page.utility.ts
+++ common/src/page/page.utility.ts
@@ -1,10 +1,14 @@
 import {Request} from "../app/request.type";
 
-export const mapRequestToPage = (request: Request) => ({
-    ...(request.queryParamMap['index'] && request.queryParamMap['limit']) && {
-        page: {
-            index: parseInt(request.queryParamMap['index']),
-            limit: parseInt(request.queryParamMap['limit'])
-        }
-    },
-});
+export const mapRequestToPage = (request: Request) => {
+    if (request.queryParamMap === undefined) return {};
+
+    return ({
+        ...(request.queryParamMap['index'] && request.queryParamMap['limit']) && {
+            page: {
+                index: parseInt(request.queryParamMap['index']),
+                limit: parseInt(request.queryParamMap['limit'])
+            }
+        },
+    });
+};
