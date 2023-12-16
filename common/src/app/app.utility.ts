4e48fef615d53ba7da9d314315ef53956250cef3
Tyrone B. Dunn <tyronebdunn@gmail.com>
2023-12-17 00:32:48 +0200

    refactor: make function total

--- common/src/app/app.utility.ts
+++ common/src/app/app.utility.ts
@@ -1,8 +1,12 @@
 import {Request} from "./request.type";
 import {Response} from "./response.type";
-export const addRequestPageDataToResponse = (request: Request, response: Response): Response =>
-    ({
+export const addRequestPageDataToResponse = (request: Request, response: Response): Response => {
+    if (request.queryParamMap === undefined)
+        return {...response};
+
+    return ({
         ...response,
         index: parseInt(request.queryParamMap['index']),
         limit: parseInt(request.queryParamMap['limit']),
     });
+};
