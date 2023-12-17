c59a5a3df0e8f9e3b81ceaad39cfc22fc1f6b8a5
Tyrone B. Dunn <tyronebdunn@gmail.com>
2023-12-17 02:01:49 +0200

    refactor: improve naming

--- common/src/error/errors.utility.ts
+++ common/src/error/errors.utility.ts
@@ -2,7 +2,7 @@ import {Response} from "../app/response.type";
 import {Error} from "./error.type"
 import {INTERNAL_SERVER_ERROR} from "../http/http-status-codes.constant";
 
-export const mapToInternalServerErrorResponse = (error: Error): Response => ({
+export const mapErrorToInternalServerErrorResponse = (error: Error): Response => ({
     status: INTERNAL_SERVER_ERROR,
     error: error.message,
 });
