4e087ce8536c2a5121d2b0fd6b2e90033fe58200
Tyrone B. Dunn <tyronebdunn@gmail.com>
2023-12-17 02:40:00 +0200

    refactor: remove duplicate code

--- core/src/shared/error-handler.service.ts
+++
@@ -1,5 +0,0 @@
-export const throwErrors = (errors: Error[]) => {
-    const errorMessages: string = '';
-    for (const error of errors) errorMessages.concat(error.message, '\n');
-    throw new Error(errorMessages);
-};
