6320a014b49d22aeb9d06762a09c175ccc4c97ce
Tyrone B. Dunn <tyronebdunn@gmail.com>
2023-12-19 21:44:36 +0200

    chore: format

--- core/src/users/mongo-user-model-config.service.ts
+++ core/src/users/mongo-user-model-config.service.ts
@@ -1,26 +1,26 @@
-import {User} from "./user.type";
-import mongoose, {Document, Model, Schema} from 'mongoose';
+import { User } from "./user.type";
+import mongoose, { Document, Model, Schema } from 'mongoose';
 
 export interface UserDocument extends Document, User {
-    _id: string,
-    username: string,
-    hash: string,
+   _id: string,
+   username: string,
+   hash: string,
 }
 
 const userSchema = new Schema<UserDocument>(
    {
-        username: {
-            type: String,
-            unique: true,
-            required: true
-        },
-        hash: {
-            type: String,
-            required: true
-        },
-    },
-   {timestamps: true}
-   );
+      username: {
+         type: String,
+         unique: true,
+         required: true,
+      },
+      hash: {
+         type: String,
+         required: true,
+      },
+   },
+   { timestamps: true },
+);
 
 export const generateUserModel = (dbUrl: string, name: string): Model<UserDocument> =>
-    mongoose.createConnection(dbUrl).model<UserDocument>(name, userSchema);
+   mongoose.createConnection(dbUrl).model<UserDocument>(name, userSchema);
