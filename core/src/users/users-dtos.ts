712cff527e3788bb315b152c539e47561f078a13
Tyrone B. Dunn <tyronebdunn@gmail.com>
2023-12-20 18:38:25 +0200

    refactor: remove unused types

--- core/src/users/users-dtos.ts
+++ core/src/users/users-dtos.ts
@@ -1,34 +1,6 @@
-import {UserSortOption} from "./user.type";
-import {OrderOption} from "../shared/order-option.type";
-
 export type GetUserDTO = {username: string};
-
-export type UsersDTO = {
-    username?: string,
-    usernameRegex?: string,
-    sort: UserSortOption,
-    order: OrderOption,
-    index: number,
-    limit: number,
-};
 
 export type RegisterUserDTO = {
     username: string,
     password: string,
-};
-
-export type UpdateUserDTO = {
-    username: string,
-    newUsername?: string,
-    newPassword?: string,
-    newIsAdmin?: string,
-    newStatus: string,
-};
-
-export type DeleteUserDTO = {username: string};
-
-export type DeleteUsersDTO = {
-    usernameRegex?: string,
-    startDate?: string,
-    endDate?: string,
 };
