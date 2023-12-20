5d665bf459ba61d7859fc26a57418710e71c4f1f
Tyrone B. Dunn <tyronebdunn@gmail.com>
2023-12-20 23:32:27 +0200

    refactor: remove schema utility

--- core/index.d.ts
+++ core/index.d.ts
@@ -1,20 +1,12 @@
 import {
-   ApplicationSchema,
-   AuthStrategy,
-   CorsOptions,
    HashAlgorithm,
    LocalStrategy,
 } from "@hals/common";
 import { DatabaseOption } from "@hals/common/lib/auth/local-strategy.type";
-import {
-   NodeEnvironmentOption,
-   WebFrameworkOption,
-} from "@hals/common/lib/app/application-schema.type";
 
 import {
    buildLocalAuthStrategy as buildLocalAuthStrategyImpl,
 } from "./src/auth/local-strategy.utility";
-import { buildSchema as buildSchemaImpl } from "./src/app/schema.utility";
 import EventEmitter from "events";
 import { HalsEventEmitter } from "./src/app/event-emitter.service";
 import {
@@ -23,18 +15,6 @@ import {
 } from "./src/app/application.utility";
 
 export const newApplication: InitialiseApplication = newApplicationImpl;
-
-export type BuildSchema = (
-   nodeEnv: NodeEnvironmentOption,
-   api: WebFrameworkOption,
-   title: string,
-   version: string,
-   port: number,
-   corsOptions: CorsOptions,
-   authStrategy: AuthStrategy,
-) => ApplicationSchema;
-
-export const buildSchema: BuildSchema = buildSchemaImpl;
 
 export type BuildLocalAuthStrategy = (
    usersDbName: string,
