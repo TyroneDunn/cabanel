b4d6fadca6ef433ac6e22970f4d1c667daa6c293
Tyrone B. Dunn <tyronebdunn@gmail.com>
2023-12-20 18:54:49 +0200

    refactor: update dependencies using common/lib

--- core/src/auth/auth-repository.utility.ts
+++ core/src/auth/auth-repository.utility.ts
@@ -1,35 +1,30 @@
-import {AuthRepository} from "./auth-repository.type";
-import {generateHash} from "../shared/password.utility";
-import {configureMongoAuthRepository} from "./mongo-auth-repository-config.service";
-import {generateUserModel} from "../users/mongo-user-model-config.service";
-import {HashingAlgorithm} from "./hashing-algorithm.type";
-import {DatabaseOption} from "./local-strategy.type";
+import { AuthRepository } from "./auth-repository.type";
+import { configureHashUtility, HashAlgorithm } from "@hals/common";
+import { configureMongoAuthRepository } from "./mongo-auth-repository-config.service";
+import { generateUserModel } from "../users/mongo-user-model-config.service";
+import { DatabaseOption } from "@hals/common/lib/auth/local-strategy.type";
 
 export const configureAuthRepository = (
-    usersDbName: string,
-    usersDbOption: DatabaseOption,
-    usersDbUrl: string,
-    passwordSalt: string,
-    passwordLength: number,
-    hashingAlgorithm: HashingAlgorithm,
-    hashingIterations: number
+   usersDbName: string,
+   usersDbOption: DatabaseOption,
+   usersDbUrl: string,
+   passwordSalt: string,
+   passwordLength: number,
+   hashingAlgorithm: HashAlgorithm,
+   hashingIterations: number,
 ): AuthRepository => {
-    const generateHashWrapper = (key: string) => generateHash(
-        key,
-        passwordSalt,
-        hashingIterations,
-        passwordLength,
-        hashingAlgorithm
-    );
-    switch (usersDbOption) {
-        case "MongoDB": {
-            return configureMongoAuthRepository(
-                generateUserModel(usersDbUrl, usersDbName),
-                generateHashWrapper,
-            );
-        }
-        default: {
-            throw new Error(`"${usersDbOption}" DB option not implemented. Please select another option.`);
-        }
-    }
+   const generateHashWrapper = (key: string) =>
+      configureHashUtility(passwordSalt, hashingIterations, passwordLength, hashingAlgorithm)
+      .generateHash(key);
+   switch (usersDbOption) {
+      case "MongoDB": {
+         return configureMongoAuthRepository(
+            generateUserModel(usersDbUrl, usersDbName),
+            generateHashWrapper,
+         );
+      }
+      default: {
+         throw new Error(`"${usersDbOption}" DB option not implemented. Please select another option.`);
+      }
+   }
 };
