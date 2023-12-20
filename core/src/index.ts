8ba0fe721b3b2be4f962a5c052ba5d0aafbd6207
Tyrone B. Dunn <tyronebdunn@gmail.com>
2023-12-20 19:08:26 +0200

    refactor: simplify interface

--- core/src/index.ts
+++ core/src/index.ts
@@ -1,19 +1,14 @@
-import {BuildSchema, buildSchema as buildSchemaImpl} from "./app/schema.utility";
 import EventEmitter from "events";
-import {HalsEventEmitter} from "./app/event-emitter.service";
+import { HalsEventEmitter } from "./app/event-emitter.service";
 import {
-    BuildLocalAuthStrategy,
-    buildLocalAuthStrategy as buildLocalAuthStrategyImpl
+   BuildLocalAuthStrategy,
+   buildLocalAuthStrategy as buildLocalAuthStrategyImpl,
 } from "./auth/local-strategy.utility";
 import {
-    InitialiseApplication,
-    newApplication as newApplicationImpl
+   InitialiseApplication,
+   newApplication as newApplicationImpl,
 } from "./app/application.utility";
 
-export const newApplication: InitialiseApplication = newApplicationImpl;
 export const buildLocalAuthStrategy: BuildLocalAuthStrategy = buildLocalAuthStrategyImpl;
-
-// todo: consider removing build schema, it is a simple process and defining it is the same as
-//  using the function
-export const buildSchema: BuildSchema = buildSchemaImpl;
 export const halsEventEmitter: EventEmitter = HalsEventEmitter;
+export const newApplication: InitialiseApplication = newApplicationImpl;
