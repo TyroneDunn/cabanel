05b0fc0008da45cd3dc42f2b8aaf573317a4c9ed
Tyrone B. Dunn <tyronebdunn@gmail.com>
2023-12-20 15:55:40 +0200

    refactor: improve error handling

--- core/src/auth/auth.service.ts
+++ core/src/auth/auth.service.ts
@@ -1,68 +1,69 @@
-import {AuthRepository} from "./auth-repository.type";
-import {GetUserDTO, RegisterUserDTO} from "../users/users-dtos";
-import {ValidationOutcome} from "../shared/validation-outcome.type";
-import {Response} from "../app/response.type";
+import { AuthRepository } from "./auth-repository.type";
+import { GetUserDTO, RegisterUserDTO } from "../users/users-dtos";
 import {
-    BAD_REQUEST,
-    CREATED,
-    INTERNAL_SERVER_ERROR,
-    NOT_FOUND,
-    OK
-} from "../shared/http-status-codes.constant";
-import {User} from "../users/user.type";
-import {validateGetUserDTO, validateRegisterUserDTO} from "../users/users-dtos-validator.utility";
+   BAD_REQUEST,
+   CREATED,
+   INTERNAL_SERVER_ERROR,
+   NOT_FOUND,
+   OK,
+   ValidationOutcome,
+} from "@hals/common";
+import { Response } from "../app/response.type";
+import { User } from "../users/user.type";
+import { validateGetUserDTO, validateRegisterUserDTO } from "../users/users-dtos-validator.utility";
 
 export type AuthService = {
-    getUser: (dto: GetUserDTO) => Promise<Response>,
-    registerUser: (dto: RegisterUserDTO) => Promise<Response>,
+   getUser: (dto: GetUserDTO) => Promise<Response>,
+   registerUser: (dto: RegisterUserDTO) => Promise<Response>,
 };
 
 export const configureAuthService = (repository: AuthRepository): AuthService => ({
-    getUser: async (dto: GetUserDTO): Promise<Response> => {
-        try {
-            const validationOutcome: ValidationOutcome =
-                await validateGetUserDTO(dto);
-            if (validationOutcome.errors.length) return mapToErrorResponse(validationOutcome);
-            const user: User | null = await repository.getUser(dto)
-            if (user) return {
-                status: OK,
-                collection: [await repository.getUser(dto)],
-                count: 1,
-            };
-            else return {
-                status: NOT_FOUND,
-                collection: [],
-                count: 0,
-            };
-        } catch (error) {
-            return {
-                status: INTERNAL_SERVER_ERROR,
-                error: 'Auth Service: "getUser" error.'
-            }
-        }
-    },
+   getUser: async (dto: GetUserDTO): Promise<Response> => {
+      try {
+         const validationOutcome: ValidationOutcome =
+            await validateGetUserDTO(dto);
+         if (validationOutcome.error) return mapToErrorResponse(validationOutcome);
+         const user: User | null = await repository.getUser(dto);
+         if (user) return {
+            status: OK,
+            collection: [ await repository.getUser(dto) ],
+            count: 1,
+         };
+         else return {
+            status: NOT_FOUND,
+            collection: [],
+            count: 0,
+         };
+      }
+      catch (error) {
+         return {
+            status: INTERNAL_SERVER_ERROR,
+            error: 'Auth Service: "getUser" error.',
+         };
+      }
+   },
 
-    registerUser: async (dto: RegisterUserDTO): Promise<Response> => {
-        try {
-            const validationOutcome: ValidationOutcome =
-                await validateRegisterUserDTO(dto);
-            if (validationOutcome.errors.length) return mapToErrorResponse(validationOutcome);
-            return {
-                status: CREATED,
-                collection: [await repository.registerUser(dto)],
-                count: 1,
-            };
-        } catch (error) {
-            return {
-                status: INTERNAL_SERVER_ERROR,
-                error: 'Auth Service: "registerUser" error.'
-            }
-        }
-    },
+   registerUser: async (dto: RegisterUserDTO): Promise<Response> => {
+      try {
+         const validationOutcome: ValidationOutcome =
+            await validateRegisterUserDTO(dto);
+         if (validationOutcome.error) return mapToErrorResponse(validationOutcome);
+         return {
+            status: CREATED,
+            collection: [ await repository.registerUser(dto) ],
+            count: 1,
+         };
+      }
+      catch (error) {
+         return {
+            status: INTERNAL_SERVER_ERROR,
+            error: 'Auth Service: "registerUser" error.',
+         };
+      }
+   },
 });
 
 export const mapToErrorResponse = (validationOutcome: ValidationOutcome): Response => ({
-    status: BAD_REQUEST,
-    error: validationOutcome.errors.reduce(
-        (message, error) => message + error.message + '\n', ''),
+   status: BAD_REQUEST,
+   error: validationOutcome.error?.message,
 });
