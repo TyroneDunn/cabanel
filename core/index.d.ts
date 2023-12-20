b078b34bd30d61a994dabd541dc8e41b96dc392e
Tyrone B. Dunn <tyronebdunn@gmail.com>
2023-12-20 23:39:48 +0200

    refactor: simplify interface

--- core/index.d.ts
+++ core/index.d.ts
@@ -1,35 +1,17 @@
 import {
-   HashAlgorithm,
-   LocalStrategy,
-} from "@hals/common";
-import { DatabaseOption } from "@hals/common/lib/auth/local-strategy.type";
-
-import {
    buildLocalAuthStrategy as buildLocalAuthStrategyImpl,
+   BuildLocalAuthStrategy as BuildLocalAuthStrategyType,
 } from "./src/auth/local-strategy.utility";
+
 import EventEmitter from "events";
 import { HalsEventEmitter } from "./src/app/event-emitter.service";
-import {
-   InitialiseApplication,
-   hals as halsImpl,
-} from "./src/app/application.utility";
+import { hals as halsImpl, InitialiseApplication } from "./src/app/application.utility";
 
-export const hals: InitialiseApplication = halsImpl;
-
-export type BuildLocalAuthStrategy = (
-   usersDbName: string,
-   usersDbOption: DatabaseOption,
-   usersDbUrl: string,
-   sessionSecret: string,
-   hashingAlgorithm: HashAlgorithm,
-   hashingIterations: number,
-   passwordLength: number,
-   passwordSalt: string,
-) => LocalStrategy;
+export type BuildLocalAuthStrategy = BuildLocalAuthStrategyType;
 
 export const buildLocalAuthStrategy: BuildLocalAuthStrategy = buildLocalAuthStrategyImpl;
+export const hals: InitialiseApplication = halsImpl;
 export const halsEventEmitter: EventEmitter = HalsEventEmitter;
-
 export const UserRegisteredEvent: string = 'userRegistered';
 export const UserLoggedInEvent: string = 'userLoggedIn';
 export const UserLoggedOutEvent: string = 'userLoggedOut';
