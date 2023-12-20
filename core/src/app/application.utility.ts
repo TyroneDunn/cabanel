e308c1f6c457ce7bd1b679c481580c330c78c910
Tyrone B. Dunn <tyronebdunn@gmail.com>
2023-12-20 15:58:01 +0200

    chore: format

--- core/src/app/application.utility.ts
+++ core/src/app/application.utility.ts
@@ -1,20 +1,20 @@
-import {Application} from "./application.type";
-import {Schema} from "./schema.type";
-import {Controller} from "./controller.type";
-import {validateAppSchema} from "./app-validator.service";
-import {ValidationOutcome} from "@hals/common";
-import {appBuilder} from "./app-builder.utility";
+import { Application } from "./application.type";
+import { Schema } from "./schema.type";
+import { Controller } from "./controller.type";
+import { validateAppSchema } from "./app-validator.service";
+import { ValidationOutcome } from "@hals/common";
+import { appBuilder } from "./app-builder.utility";
 
 export type InitialiseApplication = (
-    config: Schema,
-    controllers: Controller[]
+   config: Schema,
+   controllers: Controller[],
 ) => Application;
 
 export const newApplication: InitialiseApplication = (
-    config: Schema,
-    controllers: Controller[] = []
+   config: Schema,
+   controllers: Controller[] = [],
 ): Application => {
-    const validationOutcome: ValidationOutcome = validateAppSchema(config);
-    if (validationOutcome.error) throw new Error(validationOutcome.error.message);
-    return appBuilder.buildApp(config, controllers);
+   const validationOutcome: ValidationOutcome = validateAppSchema(config);
+   if (validationOutcome.error) throw new Error(validationOutcome.error.message);
+   return appBuilder.buildApp(config, controllers);
 };
