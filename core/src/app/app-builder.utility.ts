ad1c26b9b62045b237a0730d5ccf1848e5cb370f
Tyrone B. Dunn <tyronebdunn@gmail.com>
2024-01-01 15:49:48 +0200

    chore: format

--- core/src/app/app-builder.utility.ts
+++ core/src/app/app-builder.utility.ts
@@ -2,18 +2,22 @@ import { expressAppBuilder } from "../express/express-app-builder.utility";
 import { Application, ApplicationSchema, Controller } from "@hals/common";
 
 export type AppBuilder = {
-   buildApp: (schema: ApplicationSchema, controllers: Controller[]) => Application,
+   buildApp : (
+      schema      : ApplicationSchema,
+      controllers : Controller[]
+   ) => Application,
 };
 
-export const appBuilder: AppBuilder = {
-   buildApp(schema: ApplicationSchema, controllers: Controller[]): Application {
+export const appBuilder : AppBuilder = {
+   buildApp(
+      schema      : ApplicationSchema,
+      controllers : Controller[]
+   ) : Application {
       switch (schema.api) {
-         case "Express": {
+         case "Express":
             return expressAppBuilder.buildApp(schema, controllers);
-         }
-         default: {
+         default:
             throw new Error(`"${schema.api}" API not implemented. Please select a different option.`);
-         }
       }
    },
 };
