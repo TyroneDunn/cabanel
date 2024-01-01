8583a0c6fa5572f6a64862b1e9483c470d413501
Tyrone B. Dunn <tyronebdunn@gmail.com>
2024-01-01 16:01:06 +0200

    chore: format

--- common/lib/app/app.utility.ts
+++ common/lib/app/app.utility.ts
@@ -1,12 +1,14 @@
-import {Request} from "./request.type";
-import {Response} from "./response.type";
-export const addRequestPageDataToResponse = (request: Request, response: Response): Response => {
-    if (request.queryParamMap === undefined)
-        return {...response};
+import { Request } from "./request.type";
+import { Response } from "./response.type";
 
-    return ({
-        ...response,
-        index: parseInt(request.queryParamMap['index']),
-        limit: parseInt(request.queryParamMap['limit']),
-    });
+export const addRequestPageDataToResponse = (
+   request : Request,
+   response : Response
+) : Response => {
+   if (request.queryParamMap === undefined) return { ...response };
+   else return ({
+      ...response,
+      index : parseInt(request.queryParamMap['index']),
+      limit : parseInt(request.queryParamMap['limit']),
+   });
 };
