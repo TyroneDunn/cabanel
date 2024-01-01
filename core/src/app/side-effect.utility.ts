6edf3606314a74ef11c3b526c7bd7ad6fd6589f0
Tyrone B. Dunn <tyronebdunn@gmail.com>
2024-01-01 15:57:43 +0200

    chore: format

--- core/src/app/side-effect.utility.ts
+++ core/src/app/side-effect.utility.ts
@@ -1,4 +1,7 @@
 import { Request as HalsRequest, SideEffect as HalsSideEffect } from "@hals/common";
 
-export const executeSideEffects = (dto: HalsRequest, sideEffects: HalsSideEffect[]): void =>
-   sideEffects.forEach(sideEffect => sideEffect(dto));
+export const executeSideEffects = (
+   request         : HalsRequest,
+   sideEffects : HalsSideEffect[]
+) : void =>
+   sideEffects.forEach(sideEffect => sideEffect(request));
