3dc2cb2140010f4ce9124334be0d6fb5825e610d
Tyrone B. Dunn <tyronebdunn@gmail.com>
2024-01-02 22:19:55 +0200

    chore: format

--- core/src/auth/auth-repository.type.ts
+++ core/src/auth/auth-repository.type.ts
@@ -2,6 +2,6 @@ import { User } from "@hals/common";
 import { GetUserDTO, RegisterUserDTO } from "../users/users-dtos";
 
 export type AuthRepository = {
-   getUser: (dto: GetUserDTO) => Promise<User | null>,
-   registerUser: (dto: RegisterUserDTO) => Promise<User>,
+   getUser      : (dto : GetUserDTO) => Promise<User | null>,
+   registerUser : (dto : RegisterUserDTO) => Promise<User>,
 };
