fc4ccd5dab6d9051b63f05bbba1f70610960a083
Tyrone B. Dunn <tyronebdunn@gmail.com>
2024-01-02 22:31:10 +0200

    chore: format

--- core/src/users/users-dtos-validator.utility.ts
+++ core/src/users/users-dtos-validator.utility.ts
@@ -1,13 +1,13 @@
 import { GetUserDTO, RegisterUserDTO } from "./users-dtos";
 import { ValidationOutcome } from "@hals/common";
 
-export const validateGetUserDTO = async (dto: GetUserDTO): Promise<ValidationOutcome> => {
-   if (!dto.username) return { error: { type: "BadRequest", message: 'Username required.' } };
+export const validateGetUserDTO = async (dto : GetUserDTO) : Promise<ValidationOutcome> => {
+   if (!dto.username) return { error : { type: "BadRequest", message : 'Username required.' } };
    return {};
 };
 
-export const validateRegisterUserDTO = async (dto: RegisterUserDTO): Promise<ValidationOutcome> => {
-   if (!dto.username) return { error: { type: "BadRequest", message: 'Username required.' } };
-   if (!dto.password) return { error: { type: "BadRequest", message: 'Password required.' } };
+export const validateRegisterUserDTO = async (dto : RegisterUserDTO) : Promise<ValidationOutcome> => {
+   if (!dto.username) return { error : { type: "BadRequest", message : 'Username required.' } };
+   if (!dto.password) return { error : { type: "BadRequest", message : 'Password required.' } };
    return {};
 };
