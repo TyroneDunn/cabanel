eb7819a96baff510d04ed59bc0afee3ed7857afe
Tyrone B. Dunn <tyronebdunn@gmail.com>
2024-01-02 22:50:14 +0200

    feat: package lock

--- common/lib/app/application-schema.type.ts
+++ common/lib/app/application-schema.type.ts
@@ -2,14 +2,20 @@ import { CorsOptions } from "../cors/cors-options.type";
 import { AuthStrategy } from "../auth/auth-strategy.type";
 
 export type ApplicationSchema = {
-   nodeEnv: NodeEnvironmentOption;
-   api: WebFrameworkOption,
-   title: string,
-   version: string;
-   port: number,
-   corsOptions?: CorsOptions,
-   authStrategy: AuthStrategy,
-}
+   nodeEnv      : NodeEnvironmentOption;
+   api          : WebFrameworkOption,
+   title        : string,
+   version      : string;
+   port         : number,
+   corsOptions? : CorsOptions,
+   authStrategy : AuthStrategy,
+};
 
-export type NodeEnvironmentOption = "production" | "development";
-export type WebFrameworkOption = "Express" | "Nest" | "Fastify";
+export type NodeEnvironmentOption =
+   | "production"
+   | "development";
+
+export type WebFrameworkOption =
+   | "Express"
+   | "Nest"
+   | "Fastify";
