26eebcacaf2f82d28e7753764a3d7db5be89f848
Tyrone B. Dunn <tyronebdunn@gmail.com>
2024-01-02 22:46:26 +0200

    chore: format

--- core/index.d.ts
+++ core/index.d.ts
@@ -2,16 +2,15 @@ import {
    buildLocalAuthStrategy as buildLocalAuthStrategyImpl,
    BuildLocalAuthStrategy as BuildLocalAuthStrategyType,
 } from "./src/auth/local-strategy.utility";
-
 import EventEmitter from "events";
 import { HalsEventEmitter } from "./src/app/event-emitter.service";
 import { hals as halsImpl, InitialiseApplication } from "./src/app/application.utility";
 
 export type BuildLocalAuthStrategy = BuildLocalAuthStrategyType;
 
-export const buildLocalAuthStrategy: BuildLocalAuthStrategy = buildLocalAuthStrategyImpl;
-export const hals: InitialiseApplication = halsImpl;
-export const halsEventEmitter: EventEmitter = HalsEventEmitter;
-export const UserRegisteredEvent: string = 'userRegistered';
-export const UserLoggedInEvent: string = 'userLoggedIn';
-export const UserLoggedOutEvent: string = 'userLoggedOut';
+export const buildLocalAuthStrategy : BuildLocalAuthStrategy = buildLocalAuthStrategyImpl;
+export const hals : InitialiseApplication = halsImpl;
+export const halsEventEmitter : EventEmitter = HalsEventEmitter;
+export const UserRegisteredEvent : string = 'userRegistered';
+export const UserLoggedInEvent   : string = 'userLoggedIn';
+export const UserLoggedOutEvent  : string = 'userLoggedOut';
