5564fbef26408c7d30fb852e71ca5ec81846428a
Tyrone B. Dunn <tyronebdunn@gmail.com>
2024-01-02 22:51:56 +0200

    feat: package lock

--- common/lib/app/method.type.ts
+++ common/lib/app/method.type.ts
@@ -2,13 +2,18 @@ import { SideEffect } from "./side-effect.type";
 import { RequestHandler } from "./request-handler.type";
 
 export type Method = {
-   type: MethodType,
-   path?: string,
-   paramKeys: string[],
-   queryParamKeys: string[]
-   sideEffects: SideEffect[],
-   middleware: RequestHandler[],
-   requestHandler: RequestHandler,
+   type           : MethodType,
+   path           : string | undefined,
+   paramKeys      : string[],
+   queryParamKeys : string[]
+   sideEffects    : SideEffect[],
+   middleware     : RequestHandler[],
+   requestHandler : RequestHandler,
 };
 
-export type MethodType = "GET" | "POST" | "PUT" | "PATCH" | "DELETE";
+export type MethodType =
+   | "GET"
+   | "POST"
+   | "PUT"
+   | "PATCH"
+   | "DELETE";
