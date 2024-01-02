d381a4237df2d64c02a12ba58d9e62e1808a50e6
Tyrone B. Dunn <tyronebdunn@gmail.com>
2024-01-02 22:54:56 +0200

    feat: package lock

--- common/lib/auth/auth-strategy.type.ts
+++ common/lib/auth/auth-strategy.type.ts
@@ -1,4 +1,7 @@
 import { LocalStrategy } from "./local-strategy.type";
 import { JwtStrategy } from "./jwt-strategy.type";
 
-export type AuthStrategy = "None" | LocalStrategy | JwtStrategy;
+export type AuthStrategy =
+   | "None"
+   | LocalStrategy
+   | JwtStrategy;
--- common/lib/auth/local-strategy.type.ts
+++ common/lib/auth/local-strategy.type.ts
@@ -1,14 +1,17 @@
-import {HashAlgorithm} from "./hash-algorithm.type";
+import { HashAlgorithm } from "./hash-algorithm.type";
 
 export type LocalStrategy = {
-    usersDbName: string,
-    usersDbOption: DatabaseOption
-    usersDbUrl: string,
-    sessionSecret: string,
-    hashingAlgorithm: HashAlgorithm,
-    hashingIterations: number,
-    passwordLength: number
-    passwordSalt: string
+   usersDbName       : string,
+   usersDbOption     : DatabaseOption
+   usersDbUrl        : string,
+   sessionSecret     : string,
+   hashingAlgorithm  : HashAlgorithm,
+   hashingIterations : number,
+   passwordLength    : number
+   passwordSalt      : string
 };
 
-export type DatabaseOption = "MongoDB" | "MySQL" | "GraphQL";
+export type DatabaseOption =
+   | "MongoDB"
+   | "MySQL"
+   | "GraphQL";
