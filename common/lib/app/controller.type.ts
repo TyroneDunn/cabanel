76b2c97f2a5f31805f789066384e46e6ec856662
Tyrone B. Dunn <tyronebdunn@gmail.com>
2024-01-02 22:50:28 +0200

    feat: package lock

--- common/lib/app/controller.type.ts
+++ common/lib/app/controller.type.ts
@@ -1,7 +1,7 @@
 import { Method } from "./method.type";
 
 export type Controller = {
-   path: string,
-   guard: boolean,
-   methods: Method[],
+   path    : string,
+   guard   : boolean,
+   methods : Method[],
 };
