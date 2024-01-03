f9e6b52966adfb5262758cf0dcba2fbf99a181e3
Tyrone B. Dunn <tyronebdunn@gmail.com>
2024-01-03 22:06:23 +0200

    chore: format

--- common/lib/cors/cors-options.type.ts
+++ common/lib/cors/cors-options.type.ts
@@ -1,29 +1,21 @@
-export type StaticOrigin = boolean | string | RegExp | Array<boolean | string | RegExp>;
+export type StaticOrigin =
+   | boolean
+   | string
+   | RegExp
+   | Array<boolean | string | RegExp>;
 
 export type CustomOrigin = (
-    requestOrigin: string | undefined,
-    callback: (err: Error | null, origin?: StaticOrigin) => void,
+   requestOrigin : string | undefined,
+   callback      : (err : Error | null, origin? : StaticOrigin) => void,
 ) => void;
 
 export type CorsOptions = {
-    /**
-     * @default '*''
-     */
-    origin?: StaticOrigin | CustomOrigin | undefined;
-    /**
-     * @default 'GET,HEAD,PUT,PATCH,POST,DELETE'
-     */
-    methods?: string | string[] | undefined;
-    allowedHeaders?: string | string[] | undefined;
-    exposedHeaders?: string | string[] | undefined;
-    credentials?: boolean | undefined;
-    maxAge?: number | undefined;
-    /**
-     * @default false
-     */
-    preflightContinue?: boolean | undefined;
-    /**
-     * @default 204
-     */
-    optionsSuccessStatus?: number | undefined;
+   origin? : StaticOrigin | CustomOrigin | undefined;
+   methods? : string | string[] | undefined;
+   allowedHeaders? : string | string[] | undefined;
+   exposedHeaders? : string | string[] | undefined;
+   credentials? : boolean | undefined;
+   maxAge? : number | undefined;
+   preflightContinue? : boolean | undefined;
+   optionsSuccessStatus? : number | undefined;
 };
