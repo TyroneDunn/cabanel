d908c16d91a112308bc1aa55852865be50df28c3
Tyrone B. Dunn <tyronebdunn@gmail.com>
2024-01-03 22:07:22 +0200

    chore: format

--- common/lib/error/error.type.ts
+++ common/lib/error/error.type.ts
@@ -1,11 +1,12 @@
-export type ErrorType = | "BadRequest"
-    | "NotFound"
-    | "Unauthorized"
-    | "Conflict"
-    | "Forbidden"
-    | "Internal";
+export type ErrorType =
+   | "BadRequest"
+   | "NotFound"
+   | "Unauthorized"
+   | "Conflict"
+   | "Forbidden"
+   | "Internal";
 
 export type Error = {
-    type: ErrorType
-    message: string,
+   type    : ErrorType
+   message : string,
 };
--- common/lib/error/error.utility.ts
+++ common/lib/error/error.utility.ts
@@ -1,21 +1,20 @@
-import {Response} from "../app/response.type";
-import {Error} from "./error.type"
-import {INTERNAL_SERVER_ERROR} from "../http/http-status-codes.constant";
+import { Response } from "../app/response.type";
+import { Error } from "./error.type";
+import { INTERNAL_SERVER_ERROR } from "../http/http-status-codes.constant";
 
-export const mapErrorToInternalServerErrorResponse = (error: Error): Response => ({
-    status: INTERNAL_SERVER_ERROR,
-    error: error.message,
+export const mapErrorToInternalServerErrorResponse = (error : Error) : Response => ({
+   status : INTERNAL_SERVER_ERROR,
+   error  : error.message,
 });
 
-export const throwErrors = (errors: Error[]): void => {
-    const errorMessages: string = '';
-    for (const error of errors) errorMessages.concat(error.message, '\n');
-    throw new Error(errorMessages);
+export const throwErrors = (errors : Error[]) : void => {
+   const errorMessages : string = '';
+   for (const error of errors) errorMessages.concat(error.message, '\n');
+   throw new Error(errorMessages);
 };
 
-export const isError = (object: any): object is Error =>
-   (
-      typeof object === 'object'
-      && 'type' in object
-      && 'message' in object
-   );
+export const isError = (object : any) : object is Error => (
+   typeof object === 'object'
+   && 'type'     in object
+   && 'message'  in object
+);
