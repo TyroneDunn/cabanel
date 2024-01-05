0da679c01fb986768b43c3d79d939fb4c64ac58b
Tyrone B. Dunn <tyronebdunn@gmail.com>
2024-01-05 23:48:29 +0200

    refactor: improve error handling

--- core/src/app/app-validator.service.ts
+++ core/src/app/app-validator.service.ts
@@ -1,6 +1,6 @@
-import { ApplicationSchema, ValidationOutcome } from "@hals/common";
+import { ApplicationSchema, ValidationError } from "@hals/common";
 
-export const validateAppSchema = (schema: ApplicationSchema): ValidationOutcome => {
+export const validateAppSchema = (schema: ApplicationSchema): ValidationError | null => {
    // implement validation
-   return {};
+   return null;
 };
--- core/src/app/application.utility.ts
+++ core/src/app/application.utility.ts
@@ -1,5 +1,11 @@
 import { validateAppSchema } from "./app-validator.service";
-import { Application, ApplicationSchema, Controller, ValidationOutcome } from "@hals/common";
+import {
+   Application,
+   ApplicationSchema,
+   Controller,
+   isValidationError,
+   ValidationError,
+} from "@hals/common";
 import { appBuilder } from "./app-builder.utility";
 import { NodeEnvironmentOption } from "@hals/common/lib/app/application-schema.type";
 
@@ -12,9 +18,9 @@ export const hals : InitialiseApplication = (
    config      : ApplicationSchema,
    controllers : Controller[],
 ) : Application => {
-   const validationOutcome : ValidationOutcome = validateAppSchema(config);
-   if (validationOutcome.error) throw new Error(validationOutcome.error.message);
-   return appBuilder.buildApp(config, controllers);
+   const validationOutcome : ValidationError | null = validateAppSchema(config);
+   if (isValidationError(validationOutcome)) throw new Error(validationOutcome.error.message);
+   else return appBuilder.buildApp(config, controllers);
 };
 
 export const serverStartMessage = (
