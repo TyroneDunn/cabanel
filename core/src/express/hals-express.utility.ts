9a3f65e2a10f0f7700ddc40fb64ba227c8fd0d74
Tyrone B. Dunn <tyronebdunn@gmail.com>
2024-01-06 01:04:00 +0200

    refactor: simplify endpoint construction

--- core/src/express/hals-express.utility.ts
+++ core/src/express/hals-express.utility.ts
@@ -112,9 +112,7 @@ const appendParamKeys = (paramKeys : string[] | undefined, path : string) : stri
    if (paramKeys === undefined || paramKeys.length === 0) return path;
    else return appendParamKeys(
       paramKeys.slice(1),
-      paramKeys.length > 1
-      ? path.concat(':', paramKeys[0], '/')
-      : path.concat(':', paramKeys[0])
+      path.concat(':', paramKeys[0], '/')
    );
 };
 
