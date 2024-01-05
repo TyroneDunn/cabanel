64540d3cc1d4bb59b0b643538ce1b97e6028a316
Tyrone B. Dunn <tyronebdunn@gmail.com>
2024-01-05 19:13:33 +0200

    refactor: define optional fields

--- common/lib/app/method.type.ts
+++ common/lib/app/method.type.ts
@@ -2,13 +2,13 @@ import { SideEffect } from "./side-effect.type";
 import { RequestHandler } from "./request-handler.type";
 
 export type Method = {
-   type           : MethodType,
-   path?          : string,
-   paramKeys      : string[],
-   queryParamKeys : string[]
-   sideEffects    : SideEffect[],
-   middleware     : RequestHandler[],
-   requestHandler : RequestHandler,
+   type            : MethodType,
+   path?           : string,
+   paramKeys?      : string[],
+   queryParamKeys? : string[]
+   sideEffects?    : SideEffect[],
+   middleware?     : RequestHandler[],
+   requestHandler  : RequestHandler,
 };
 
 export type MethodType =
