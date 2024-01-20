201e416b9dff965c0f1c6f921c99ad6d9c965041
Tyrone B. Dunn <tyronebdunn@gmail.com>
2024-01-20 21:31:49 +0200

    refactor: improve naming

--- common/lib/app/app.utility.ts
+++ common/lib/app/app.utility.ts
@@ -18,9 +18,9 @@ export const addPageDataToResponse = (
 export const handleRequest = async <T>(
    request: T,
    validate: (request: T) => Promise<ValidationError | null>,
-   handler: (request: T) => Promise<Response>
+   reduce: (request: T) => Promise<Response>
 ): Promise<Response> => {
    const validationOutcome : ValidationError | null = await validate(request);
    if (isValidationError(validationOutcome)) return mapValidationErrorToErrorResponse(validationOutcome);
-   else return handler(request);
-}
+   else return reduce(request);
+};
