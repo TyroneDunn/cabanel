91133dc3152c0037e77e75730eb144e4e6fc4454
Tyrone B. Dunn <tyronebdunn@gmail.com>
2024-01-20 21:28:30 +0200

    refactor: make application schema type class

--- common/lib/app/application-schema.type.ts
+++ common/lib/app/application-schema.type.ts
@@ -2,15 +2,9 @@ import { CorsOptions } from "../cors/cors-options.type";
 import { AuthStrategy } from "../auth/auth-strategy.type";
 import { Controller } from './controller.type';
 
-export type ApplicationSchema = {
-   nodeEnv      : NodeEnvironmentOption;
-   serverOption : ServerFrameworkOption,
-   title        : string,
-   version      : string;
-   port         : number,
-   corsOptions? : CorsOptions,
-   authStrategy : AuthStrategy,
-};
+export type ApplicationSchema =
+   | RestApplicationSchema
+   | WebSocketApplicationSchema;
 
 export type RestApplicationSchema = {
    title        : string,
