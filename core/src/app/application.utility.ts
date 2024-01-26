22b90c626467b71909ac10dae273fc8d1fe0e5b1
Tyrone B. Dunn <tyronebdunn@gmail.com>
2024-01-26 21:20:44 +0200

    refactor: extract server started message

--- core/src/app/application.utility.ts
+++ core/src/app/application.utility.ts
@@ -22,25 +22,3 @@ export const hals : InitialiseApplication = (
    if (isValidationError(validationOutcome)) throw new Error(validationOutcome.error.message);
    else return appBuilder.buildApp(config, controllers);
 };
-
-export const serverStartMessage = (
-   title       : string,
-   port        : number,
-   version     : string,
-   environment : NodeEnvironmentOption
-) : string => {
-   const messageTitle   : string = `'${title}' Hals Server Started\n `;
-   const messageDivider : string = '----------------------------------------\n';
-   const messageActions : string = `App running at http://localhost:${port}\n` +
-                                   `Press Ctrl+C to stop the server.\n`;
-   return messageTitle + messageDivider + serverMetadata(title, port, version, environment) + '\n' +
-          messageActions + messageDivider;
-};
-
-export const serverMetadata = (
-   title       : string,
-   port        : number,
-   version     : string,
-   environment : NodeEnvironmentOption
-) : string =>
-   `Server: ${title}\nPort: ${port}\nEnvironment: ${environment}\nVersion: ${version}\n`;
---
+++ core/src/app/server-started-message.utility.ts
@@ -0,0 +1,24 @@
+import { NodeEnvironmentOption } from '@hals/common/lib/app/application-schema.type';
+
+export const serverStartedMessage = (
+   title : string,
+   host : string,
+   port : number,
+   version : string,
+   environment : NodeEnvironmentOption,
+) : string => {
+   const messageTitle : string = `'${title}' Hals Server Started\n `;
+   const messageDivider : string = '----------------------------------------\n';
+   const messageActions : string = `App running at http://${host}:${port}\n` +
+      `Press Ctrl+C to stop the server.\n`;
+   return messageTitle + messageDivider + serverMetadata(title, port, version, environment) + '\n' +
+      messageActions + messageDivider;
+};
+
+const serverMetadata = (
+   title : string,
+   port : number,
+   version : string,
+   environment : NodeEnvironmentOption,
+) : string =>
+   `Server: ${title}\nPort: ${port}\nEnvironment: ${environment}\nVersion: ${version}\n`;
