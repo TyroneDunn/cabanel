96d3bc41bd3c40a674a6463bab582b63486b6746
Tyrone B. Dunn <tyronebdunn@gmail.com>
2024-02-02 10:51:39 +0200

    feat: add user to request type

--- common/lib/app/request.type.ts
+++ common/lib/app/request.type.ts
@@ -1,7 +1,9 @@
 import { ParamMap } from "./param-map.type";
+import { User } from '../users/user.type';
 
 export type Request = {
    paramMap      : ParamMap,
    queryParamMap : ParamMap,
    payload       : any,
+   user?         : User,
 };
