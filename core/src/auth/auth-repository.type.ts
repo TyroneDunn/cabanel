b4c81f65711b817b9cb1b70d1b62e51d6b7ad1fd
Tyrone B. Dunn <tyronebdunn@gmail.com>
2024-02-24 23:41:38 +0200

    refactor: improve error handling

--- core/src/auth/auth-repository.type.ts
+++ core/src/auth/auth-repository.type.ts
@@ -1,7 +1,10 @@
-import { User } from "@hals/common";
+import { User, Error, CommandResult } from "@hals/common";
 import { GetUserDTO, RegisterUserDTO } from "../users/users-dtos";
 
 export type AuthRepository = {
-   getUser      : (dto : GetUserDTO) => Promise<User | null>,
-   registerUser : (dto : RegisterUserDTO) => Promise<User>,
+   getUser      : (dto : GetUserDTO) => Promise<User | Error>,
+   registerUser : (dto : RegisterUserDTO) => Promise<User | Error>,
+   exists       : UserExists,
 };
+
+export type UserExists = (username : string) => Promise<boolean | Error>;
