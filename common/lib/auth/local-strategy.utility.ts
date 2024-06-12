f3cb3f74d7a491866ee5bd07b6b1f78636831397
Tyrone B. Dunn <tyronebdunn@gmail.com>
2024-06-12 22:30:45 +0200

    refactor: update return type, make equivalence test explicit

--- common/lib/auth/local-strategy.utility.ts
+++ common/lib/auth/local-strategy.utility.ts
@@ -1,6 +1,7 @@
 import { AuthStrategy } from "./auth-strategy.type";
+import { LocalStrategy } from './local-strategy.type';
 
-export const isLocalStrategy = (authStrategy : AuthStrategy) => (
+export const isLocalStrategy = (authStrategy : AuthStrategy) : authStrategy is LocalStrategy => (
    typeof authStrategy    === 'object'
    && 'usersDbName'       in authStrategy
    && 'usersDbOption'     in authStrategy
