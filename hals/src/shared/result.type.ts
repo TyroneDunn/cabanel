c7614f523a3f50cff44677625460eab29223261e
Tyrone B. Dunn <tyronebdunn@gmail.com>
2024-06-13 00:10:48 +0200

    feat: define result data type

---
+++ hals/src/shared/result.type.ts
@@ -0,0 +1,16 @@
+export type Result<T, U> =
+   | Success<T>
+   | Failure<U>;
+
+export type Success<T> = { data: T };
+export type Failure<U> = { error: U };
+
+export const isSuccess = (result : Result<any,any>) : result is Success<any> => (
+   typeof result === 'object'
+   && 'data' in result
+);
+
+export const isFailure = (result : Result<any,any>) : result is Failure<any> => (
+   typeof result === 'object'
+   && 'error' in result
+);
