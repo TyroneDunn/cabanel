6b246a9926fb83f0778a3dd3e43de3aaec1c7d99
Tyrone B. Dunn <tyronebdunn@gmail.com>
2024-06-13 12:35:14 +0200

    feat: defined initialise application functions

--- hals/src/application/application.ts
+++ hals/src/application/application.ts
@@ -1,14 +1,16 @@
 import {
    RestServerApplication,
    RestServerApplicationSchema,
+   isRestServerApplicationSchema
 } from './rest-server-application';
 import {
    WebSocketServerApplicationSchema,
    WebSocketServerApplication,
+   isWebSocketServerApplicationSchema,
 } from './web-socket-server-application';
 import { LocalAuthStrategy } from './local-auth-strategy';
 import { JwtAuthStrategy } from './jwt-auth-strategy';
-import { Result } from '../common/result';
+import { Result, Success, isFailure } from '../common/result';
 import { ValidationError } from '../common/validation';
 
 export type Application =
@@ -18,6 +20,13 @@ export type Application =
 export type ApplicationSchema =
    | RestServerApplicationSchema
    | WebSocketServerApplicationSchema;
+
+export type InitialiseApplication = (schema : ApplicationSchema) => Application;
+
+type ValidateApplicationSchema =
+   (schema : ApplicationSchema) => Result<undefined, ValidationError>;
+
+type BuildApplication = (schema : ApplicationSchema) => Application;
 
 export type NodeEnvironmentOption =
    | "production"
@@ -31,5 +40,36 @@ export type AuthStrategy =
    | LocalAuthStrategy
    | JwtAuthStrategy;
 
-export type ValidateApplicationSchema =
-   (schema : ApplicationSchema) => Result<undefined, ValidationError>;
+
+export const hals : InitialiseApplication = (schema : ApplicationSchema) : Application => {
+   const validationResult : Result<undefined, ValidationError> = validateApplicationSchema(schema);
+   if (isFailure(validationResult))
+      throw new Error(validationResult.error.message);
+
+   else
+      return buildApplication(schema);
+};
+
+const validateApplicationSchema : ValidateApplicationSchema =
+   (schema : ApplicationSchema): Result<undefined, ValidationError> => {
+      // IMPLEMENT
+      const stubResult : Success<undefined> = { data: undefined };
+      return stubResult;
+   };
+
+export const buildApplication: BuildApplication = (schema : ApplicationSchema) : Application => {
+   if (isWebSocketServerApplicationSchema(schema))
+      // IMPLEMENT
+      throw new Error(
+         'Web Socket Application not implemented. Please select a different option.',
+      );
+   if (isRestServerApplicationSchema(schema)) {
+      switch (schema.serverOption) {
+         case "Express": return buildExpressRestServerApplication(schema);
+         default: throw new Error(
+            `"${schema.serverOption}" REST server application not supported. Please select a different option.`
+         );
+      }
+   }
+   throw new Error("Invalid application schema definition.");
+};
