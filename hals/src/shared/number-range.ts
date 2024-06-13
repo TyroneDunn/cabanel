b013d3c7fcb102085eccf7b85d64a39d2eefb8f6
Tyrone B. Dunn <tyronebdunn@gmail.com>
2024-06-13 10:48:56 +0200

    chore: improve format

--- hals/src/shared/number-range.ts
+++ hals/src/shared/number-range.ts
@@ -5,6 +5,13 @@ export type NumberRange =
 
 export type StartOnly = { start : number };
 
+export type EndOnly = { end: number };
+
+export type StartAndEnd = {
+   start : number,
+   end   : number,
+};
+
 export const isStartOnly = (range : NumberRange) : range is StartOnly => (
    range !== null
    && typeof range === 'object'
@@ -12,19 +19,12 @@ export const isStartOnly = (range : NumberRange) : range is StartOnly => (
    && !('end'     in range)
 );
 
-export type EndOnly = { end: number };
-
 export const isEndOnly = (range : NumberRange) : range is EndOnly => (
    range !== null
    && typeof range === 'object'
    && 'end'        in range
    && !('start'    in range)
 );
-
-export type StartAndEnd = {
-   start : number,
-   end   : number,
-};
 
 export const isStartAndEnd = (range : NumberRange) : range is StartAndEnd => (
    range !== null
