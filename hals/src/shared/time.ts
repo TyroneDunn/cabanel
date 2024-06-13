184bc79e5f7f7798ee645d2090e5e7d45b4ede08
Tyrone B. Dunn <tyronebdunn@gmail.com>
2024-06-13 10:54:48 +0200

    feat: define time domain

---
+++ hals/src/shared/time.ts
@@ -0,0 +1,41 @@
+export type DateRange =
+   | StartOnlyDateRange
+   | EndOnlyDateRange
+   | StartAndEndDateRange;
+
+export type StartOnlyDateRange = {
+   start : string,
+};
+
+export type EndOnlyDateRange = {
+   end   : string,
+};
+
+export type StartAndEndDateRange = {
+   start : string,
+   end   : string,
+};
+
+export const isStartOnlyDateRange = (range : DateRange) : range is StartOnlyDateRange => (
+   range !== null
+   && typeof range === 'object'
+   && 'start'     in range
+   && !('end'     in range)
+);
+
+export const isEndOnlyDateRange = (range : DateRange) : range is EndOnlyDateRange => (
+   range !== null
+   && typeof range === 'object'
+   && 'end'     in range
+   && !('start'     in range)
+);
+
+export const isStartAndEndDateRange = (range : DateRange) : range is StartAndEndDateRange => (
+   range !== null
+   && typeof range === 'object'
+   && 'start'      in range
+   && 'end'        in range
+);
+
+export type CreatedAtDateRange = DateRange;
+export type UpdatedAtDateRange = DateRange;
