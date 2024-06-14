5e7e68e9111437623042f7fe6cfbd1e1f9d5af1b
Tyrone B. Dunn <tyronebdunn@gmail.com>
2024-06-14 16:33:33 +0200

    refactor: update configure local authentication mongo implementation

--- hals/src/express/authentication.ts
+++ hals/src/express/authentication.ts
@@ -9,7 +9,12 @@ import { HttpResponse, unauthorized } from '../http/http';
 import { isLocalAuthStrategy } from '../application/local-auth-strategy';
 import { configureLocalAuthentication } from './local-authentication';
 import { AuthStrategy, NodeEnvironmentOption } from '../application/application';
-import { validateHash } from '../password/password';
+import { generateHash, validateHash } from '../password/password';
+import {
+   getUserFromMongodb,
+   initialiseMongoUsersModel,
+   registerUserInMongodb,
+} from '../users/mongo-users';
 
 
 export type ConfigureExpressRestServerApplicationAuthentication = (
@@ -26,17 +31,34 @@ export const configureExpressRestServerApplicationAuthentication: ConfigureExpre
    if (authStrategy === "None") return;
 
    if (isLocalAuthStrategy(authStrategy)) {
-      configureLocalAuthentication(
-         app,
-         authStrategy.registerUser,
-         authStrategy.getUser,
-         validateHash(authStrategy.passwordOptions),
-         authStrategy.sessionSecret,
-         environment,
-         authStrategy.databaseOptions.databaseProvider,
-         authStrategy.databaseOptions.databaseUrl
-      );
-      return;
+      switch (authStrategy.databaseOptions.databaseProvider) {
+         case "MongoDB": {
+            const usersModel = initialiseMongoUsersModel(
+               authStrategy.databaseOptions.databaseUrl,
+               authStrategy.databaseOptions.usersDatabaseName);
+            configureLocalAuthentication(
+               app,
+               registerUserInMongodb(
+                  usersModel,
+                  generateHash(authStrategy.passwordOptions)),
+               getUserFromMongodb(usersModel),
+               validateHash(authStrategy.passwordOptions),
+               authStrategy.sessionSecret,
+               environment,
+               authStrategy.databaseOptions.databaseProvider,
+               authStrategy.databaseOptions.databaseUrl
+            );
+            break;
+         }
+         case "MySQL": {
+            throw new Error('MySQL database provider not supported. Please select another' +
+               ' option');
+         }
+         case "GraphQL": {
+            throw new Error('GraphQL database provider not supported. Please select another' +
+               ' option');
+         }
+      }
    }
 
    // IMPLEMENT JWT strategy etc.
