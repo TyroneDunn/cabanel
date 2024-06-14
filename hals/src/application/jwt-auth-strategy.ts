f0ce496335a15c12e36c7e9c1b0d660eb2466969
Tyrone B. Dunn <tyronebdunn@gmail.com>
2024-06-14 16:34:10 +0200

    refactor: remove user function dependencies

--- hals/src/application/jwt-auth-strategy.ts
+++ hals/src/application/jwt-auth-strategy.ts
@@ -1,6 +1,1 @@
-import { GetUser, RegisterUser } from '../users/users';
-
-export type JwtAuthStrategy = {
-   getUser : GetUser,
-   registerUser : RegisterUser
-};
+export type JwtAuthStrategy = { };
