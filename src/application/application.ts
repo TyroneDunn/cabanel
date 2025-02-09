fa1deea7cf4afb468e70214489039f8f29f4a015
dunnt <tyronebdunn@gmail.com>
2025-02-09 11:15:26 +0200

    refactor: simplify definition

--- src/application/application.ts
+++ src/application/application.ts
@@ -1,12 +1,12 @@
 import {
-   isRestServerApplicationSchema,
-   RestServerApplicationSchema,
+  isRestServerApplicationSchema,
+  RestServerApplicationSchema,
 } from './rest-server-application';
 import {
-   isWebSocketServerApplicationSchema,
-   WebSocketServerApplicationSchema,
+  isWebSocketServerApplicationSchema,
+  WebSocketServerApplicationSchema,
 } from './web-socket-server-application';
-import { isLocalAuthStrategy, LocalAuthStrategy } from './local-auth-strategy';
+import { LocalAuthStrategy } from './local-auth-strategy';
 import { JwtAuthStrategy } from './jwt-auth-strategy';
 import { isFailure, Result } from '../common/result';
 import { ValidationError } from '../common/validation';
@@ -16,112 +16,96 @@ import { HttpRequest } from '../http/http';
 import { httpRequestToStdOutLogger } from "../log/log";
 
 export type Application = {
-   run: () => void
+  run: () => void
 };
 
 export type ApplicationSchema =
-   | RestServerApplicationSchema
-   | WebSocketServerApplicationSchema;
+  | RestServerApplicationSchema
+  | WebSocketServerApplicationSchema;
 
 export type InitialiseApplication = (schema : ApplicationSchema) => Application;
 
 type ValidateApplicationSchema =
-   (schema : ApplicationSchema) => Result<undefined, ValidationError>;
+  (schema : ApplicationSchema) => Result<undefined, ValidationError>;
 
 type BuildApplication = (schema : ApplicationSchema) => Application;
 
 export type NodeEnvironmentOption =
-   | "production"
-   | "development";
+  | "production"
+  | "development";
 
 export type ServerFrameworkOption =
-   | "Express"
+  | "Express"
 
 export type AuthStrategy =
-   | "None"
-   | LocalAuthStrategy
-   | JwtAuthStrategy;
+  | "None"
+  | LocalAuthStrategy
+  | JwtAuthStrategy;
 
 
 export const cabanel : InitialiseApplication = (schema : ApplicationSchema) : Application => {
-   const validationResult : Result<undefined, ValidationError> = validateApplicationSchema(schema);
-   if (isFailure(validationResult))
-      throw new Error(validationResult.error.message);
+  const validationResult : Result<undefined, ValidationError> = validateApplicationSchema(schema);
+  if (isFailure(validationResult))
+    throw new Error(validationResult.error.message);
 
-   else {
-      httpRequestToStdOutLogger(httpRequestSubject.asObservable());
-      return buildApplication(schema);
-   }
+  else {
+    httpRequestToStdOutLogger(httpRequestSubject.asObservable());
+    return buildApplication(schema);
+  }
 };
 
 export const validateApplicationSchema : ValidateApplicationSchema =
-   (schema : ApplicationSchema): Result<undefined, ValidationError> => {
-      // IMPLEMENT
-      if (isRestServerApplicationSchema(schema)) {
-         if (schema.host === '')
-            return { error: { message: 'Host cannot be empty string', type: 'BadRequest' } };
-      }
-      return { data: undefined };
-   };
+  (schema : ApplicationSchema): Result<undefined, ValidationError> => {
+    // IMPLEMENT
+    if (isRestServerApplicationSchema(schema)) {
+      if (schema.host === '')
+        return { error: { message: 'Host cannot be empty string', type: 'BadRequest' } };
+    }
+    return { data: undefined };
+  };
 
 const buildApplication: BuildApplication = (schema : ApplicationSchema) : Application => {
-   if (isWebSocketServerApplicationSchema(schema))
-      // IMPLEMENT
-      throw new Error(
-         'Web Socket Application not yet implemented. Please select a' +
-        ' different option.' );
+  if (isWebSocketServerApplicationSchema(schema))
+    // IMPLEMENT
+    throw new Error(
+      'Web Socket Application not yet implemented. Please select a' +
+      ' different option.' );
 
-   if (isRestServerApplicationSchema(schema))
-      return buildExpressRestServerApplication(schema);
+  if (isRestServerApplicationSchema(schema))
+    return buildExpressRestServerApplication(schema);
 
-   throw new Error("Invalid application schema definition.");
+  throw new Error("Invalid application schema definition.");
 };
 
 export const serverStartupMessage = (
-   title: string,
-   host: string,
-   port: number,
-   version: string,
-   environment: NodeEnvironmentOption,
-   authStrategy: AuthStrategy,
+  title: string,
+  host: string,
+  port: number,
 ): string => {
-   const headline: string = `'${title}' server started.\n`;
-   const authStrategyString: string | undefined = isLocalAuthStrategy(authStrategy) ? 'local' : undefined;
-   return headline + serverMetadata(host, port, environment, version, authStrategyString);
+  const headline: string = `'${title}' server started.\n`;
+  return headline + serverMetadata(host, port);
 };
 
 export type RenderJsonServerMetadata = (
-   title : string,
-   port : number,
-   version : string,
-   environment : NodeEnvironmentOption,
+  title : string,
+  port : number,
 ) => object;
 
 export const renderJsonServerMetadata : RenderJsonServerMetadata = (
-   title : string,
-   port : number,
-   version : string,
-   environment : NodeEnvironmentOption,
+  title : string,
+  port : number,
 )  => ({
-   title      : title,
-   port       : port,
-   environment: environment,
-   version    : version,
+  title      : title,
+  port       : port,
 });
 
 export const serverMetadata = (
-   host: string,
-   port: number,
-   environment: NodeEnvironmentOption,
-   version: string,
-   authStrategy?: string,
-) =>
-    `url: http://${host}:${port}\n` +
-    `environment: ${environment}\n` +
-    `${!!authStrategy ? `auth strategy: ${authStrategy}\n`: ''}` +
-    `version: ${version}\n`;
+  host: string,
+  port: number )
+: string =>
+    `url: http://${host}:${port}\n`;
 
 export type HttpRequest$ = Observable<HttpRequest | undefined>;
 
 export const httpRequestSubject : BehaviorSubject<HttpRequest | undefined> =
-   new BehaviorSubject<HttpRequest | undefined>(undefined);
+  new BehaviorSubject<HttpRequest | undefined>(undefined);
